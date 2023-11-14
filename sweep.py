451f3a9d4307f1f1178b8af656893f569e81e0e5
Joseph123231 <144241862+Joseph123231@users.noreply.github.com>
2023-11-14 02:45:51 +0000

    Working

--- sweep.py
+++ sweep.py
@@ -4,17 +4,44 @@
 # Return value should be a string that says either Valid or Invalid (with some hints as to why it's invlaid)
 def validate( block_data ):
   # Check whether the centre block is a bomb, a number, or an invalid input
-  # Skip bombs, send an error on invalid input, verify number
-  # NOTE check if the sum of all the numebers in the grid equals the middle block if it does it's true else it's false
+  # Skip bombs, send an error on invalid input, verify numbers
 
-
-print(grid [1][1])
+  return validation
 
 grid = [
-  [-1,1,1],
+  [0,0,0],
   [1,1,0],
-  [0,0,0]
+  [-1,1,0]
 ]
 
+row = 0
+col = 0 
+bombs = 0
+
+while row < 3:
+    while col < 3: 
+#        print("here")
+        value = grid [row][col]
+#        print(row)
+#        print(col)
+#        print(value)
+        if value == -1:
+            bombs = bombs + 1
+            col = col + 1
+#            print(col)
+        else:
+            col = col + 1
+    row = row + 1
+    col = 0    
+#print(f"the number of bombs are {bombs}")
+
+middle_value = grid [1][1]
+#print(f"The middle value is {middle_value}")
+
+if middle_value == bombs:
+#    print(f"VALID")
+    validation = "VALID"
+else:
+#    print(f"INVALID")
+    validation = "INVALID"
 print (validate(grid))
-return
